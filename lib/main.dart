e1917bff5b6127206c0bab0bbac41cfc22e82e0f
MoHaMeD-Sa3eD <mahmedsaed73@gmail.com>
2024-02-17 14:49:59 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/screens/HomeScreen.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+   home: HomeScreen(),
+    );
+  }
+}
