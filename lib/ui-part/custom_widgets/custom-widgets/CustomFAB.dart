369cbe88d634a7f0477e739bf58a9ee2e2c276f8
MoHaMeD-Sa3eD <mahmedsaed73@gmail.com>
2024-02-17 19:37:09 +0200

    changed decoration of the bottomSheet

--- lib/ui-part/custom_widgets/custom-widgets/CustomFAB.dart
+++ lib/ui-part/custom_widgets/custom-widgets/CustomFAB.dart
@@ -29,7 +29,7 @@ class CustomFAB extends StatelessWidget {
         return Container();
       },
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16)
+        borderRadius: BorderRadius.circular(24)
       ),
     );
   }
